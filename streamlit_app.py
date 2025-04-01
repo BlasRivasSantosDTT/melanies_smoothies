43c7ceafe88d24aae7225c85e623a5cab26497b7
BlasRivasSantosDTT <brivassantos@deloitte.es>
2025-04-01 17:04:47 +0200

    Update streamlit_app.py
    move things

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -47,8 +48,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, friend {name_on_order}', icon="✅")
 
-# New section to display smoothiefroot nutrition information
-import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
